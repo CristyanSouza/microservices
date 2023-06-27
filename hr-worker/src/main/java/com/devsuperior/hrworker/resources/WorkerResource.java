2f6f7cce03feafc7acd33657d7b3576bf1fce465
Cristyan de Souza <cristyansouzas2@gmail.com>
2023-06-26 22:53:31 -0300

    hr-worker client config-server

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,10 +2,9 @@ package com.devsuperior.hrworker.resources;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,35 +15,33 @@ import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
 
 @RestController
+@RefreshScope
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-	
-	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
-	@Autowired
-	private Environment env;
-	
+
+	@Value("${test.config}")
+	private String testConfig;
+
 	@Autowired
 	private WorkerRepository repository;
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
+		System.out.println("CONFIGURAÇÃO: " + testConfig);
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}	
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-		
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		logger.info("PORT = " + env.getProperty("local.server.port"));
-		
+				
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
+	}	
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<String> config() {
+		System.out.println("CONFIGURAÇÃO: " + testConfig);
+		return ResponseEntity.ok(testConfig);
 	}	
 }
