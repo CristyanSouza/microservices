53c9eb895a88d910c7431cb8afd6a32abec7cedd
Cristyan de Souza <cristyansouzas2@gmail.com>
2023-06-26 21:38:13 -0300

    hr-config-server settings

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,4 @@
 package com.devsuperior.hrconfigserver;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
